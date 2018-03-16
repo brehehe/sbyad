31378465a59c1c62156cd831325d62bb802379f7
Optimus-PC\Optimus <bratang1h@gmail.com>
2018-03-16 10:57:31 +0700

    no message

---
+++ js/costum-all.js
@@ -0,0 +1,16 @@
+var b = jQuery(".main-header-wrapper").offset().top;
+jQuery(window).on("load scroll", function () {
+	var c = jQuery(this).scrollTop();
+	console.log("b = " + b);
+	console.log("c = " + c);
+	if (c > b) {
+		// jos
+		// jQuery("#main-header-wrapper").removeClass('bgtrp');
+		// jQuery("#main-header-wrapper").addClass('bgorg');
+		jQuery("#main-header-wrapper").css({background: '#333'})
+	} else {
+		// jQuery("#main-header-wrapper").removeClass('bgorg');
+		// jQuery("#main-header-wrapper").addClass('bgtrp');
+		jQuery("#main-header-wrapper").css({background: '#333'})
+	}
+})
